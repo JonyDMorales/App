7cad0e5341c9705ee9bac95c117ca15335a7c28c
IntegraDev <integradev@iMac-de-Alexis.local>
2018-02-19 19:10:12 -0600

    Hola

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    rootPage:string = 'startPage';
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
+  
+    }
 
-  }
+    goToOtherPage() {
+        //push another page onto the history stack
+        //causing the nav controller to animate the new page in
+        this.navCtrl.push('AboutPage');
+      }
 
 }
